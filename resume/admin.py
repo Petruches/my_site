f7a5ec4db76de50a3d90a2f2e6f543e60b695e5a
p.bykov <p.bykov@qiwi.com>
2025-02-21 18:34:34 +0300

    fix comment

--- resume/admin.py
+++ resume/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Experience
 
-# admin.site.register(Experience)adsads
+# admin.site.register(Experience)
 @admin.register(Experience)
 class ExperienceAdmin(admin.ModelAdmin):
     list_Experience = ('title', 'content', 'is_published')
