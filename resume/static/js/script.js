bf1c538dd1272cb29012e5a1717d5d65cb2213f3
Petruches <petya.bykov.99@mail.ru>
2025-04-27 20:13:53 +0300

    add time

--- resume/static/js/script.js
+++ resume/static/js/script.js
@@ -1,4 +1,13 @@
 var d = new Date();
 var n = d.toLocaleTimeString();
 
-console.log(n)
+//console.log(n)
+
+function time() {
+    let clock = document.getElementById('clock');
+    setInterval(function() {
+        let dt = new Date();
+        clock.innerText = dt.toLocaleTimeString();
+    }, 1000)
+};
+time()
